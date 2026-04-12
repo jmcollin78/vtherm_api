da060e35dcbcf785661ab6955c44629b54c6342a
Jean-Marc Collin <jean-marc.collin-extern@renault.com>
2026-04-12 14:49:49 +0000

    Improve vtherm_api

--- src/vtherm_api/const.py
+++ src/vtherm_api/const.py
@@ -1,6 +1,29 @@
+""" Some constants for the VThermAPI library """
+
+from datetime import datetime
+from homeassistant.util import dt as dt_util
+from homeassistant.core import HomeAssistant
 DEVICE_MANUFACTURER = "JMCOLLIN"
 DEVICE_MODEL = "Versatile Thermostat"
 
 DOMAIN = "versatile_thermostat"
 
 VTHERM_API_NAME = "vtherm_api"
+
+
+def get_tz(hass: HomeAssistant):
+    """Get the current timezone"""
+
+    return dt_util.get_time_zone(hass.config.time_zone)
+
+
+class NowClass:
+    """For testing purpose only"""
+
+    @staticmethod
+    def get_now(hass: HomeAssistant) -> datetime:
+        """A test function to get the now.
+        For testing purpose this method can be overriden to get a specific
+        timestamp.
+        """
+        return datetime.now(get_tz(hass))
--- src/vtherm_api/vtherm_api.py
+++ src/vtherm_api/vtherm_api.py
@@ -1,4 +1,5 @@
-
+""" The API of Versatile Thermostat"""
+from datetime import datetime
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, VTHERM_API_NAME
@@ -7,28 +8,67 @@ from .log_collector import get_vtherm_logger
 _LOGGER = get_vtherm_logger(__name__)
 
 
-class VersatileThermostatAPI:
-    """The VersatileThermostatAPI"""
+class VThermAPI:
+    """The VThermAPI
+    This class is a singleton that provides access to the VTherm API instance through HA hass mecanism. It also provides a reference to the HA instance for use in the API implementation.
+    """
 
     _hass: HomeAssistant = None
+
+    def __init__(self) -> None:
+        """Initialize the VThermAPI instance."""
+        super().__init__()
+        self._now: datetime = None
 
     @classmethod
     def get_vtherm_api(cls, hass=None):
         """Get the eventual VTherm API class instance or
         instantiate it if it doesn't exists"""
         if hass is not None:
-            VersatileThermostatAPI._hass = hass
+            VThermAPI._hass = hass
 
-        if VersatileThermostatAPI._hass is None:
+        if VThermAPI._hass is None:
             return None
 
-        domain = VersatileThermostatAPI._hass.data.get(DOMAIN)
+        domain = VThermAPI._hass.data.get(DOMAIN)
         if not domain:
-            VersatileThermostatAPI._hass.data.setdefault(DOMAIN, {})
+            VThermAPI._hass.data.setdefault(DOMAIN, {})
 
-        ret = VersatileThermostatAPI._hass.data.get(
+        ret = VThermAPI._hass.data.get(
             DOMAIN).get(VTHERM_API_NAME)
         if ret is None:
-            ret = VersatileThermostatAPI()
-            VersatileThermostatAPI._hass.data[DOMAIN][VTHERM_API_NAME] = ret
+            ret = VThermAPI()
+            VThermAPI._hass.data[DOMAIN][VTHERM_API_NAME] = ret
         return ret
+
+    @classmethod
+    def reset_vtherm_api(cls):
+        """Reset the VTherm API instance and related data."""
+        if VThermAPI._hass is None:
+            return
+
+        # Remove the API instance from hass.data
+        if DOMAIN in VThermAPI._hass.data:
+            VThermAPI._hass.data[DOMAIN].pop(
+                VTHERM_API_NAME, None)
+        VThermAPI._hass = None
+
+    @property
+    def hass(self):
+        """Get the HomeAssistant object"""
+        return VThermAPI._hass
+
+    @property
+    def name(self) -> str:
+        """Get the name of the API"""
+        return "VThermAPI"
+
+    # For testing purpose
+    def _set_now(self, now: datetime):
+        """Set the now timestamp. This is only for tests purpose"""
+        self._now = now
+
+    @property
+    def now(self) -> datetime:
+        """Get now. The local datetime or the overloaded _set_now date"""
+        return self._now if self._now is not None else NowClass.get_now(self._hass)
